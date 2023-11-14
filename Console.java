6644d42077935fdd509da38333402abc9c94f160
Shreyon Roy <shreyon312@gmail.com>
2023-11-14 15:09:26 -0600

    Add Basic login command

--- Console.java
+++ Console.java
@@ -1,1 +1,16 @@
 //For the "frontend"
+
+import java.util.Scanner;
+public class Console {
+
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        //java.io.Console console = System.console();
+        System.out.println("Please enter username: ");
+        //String username = console.readLine("Username: ");
+        String username = input.nextLine();
+        System.out.println("Please enter password: ");
+        //String password = new String(console.readPassword("Password: "));
+        String password = input.nextLine();
+    }
+}
