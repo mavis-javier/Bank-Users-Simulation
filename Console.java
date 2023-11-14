f675db6f6da115d238d1787b6247136c00b73e20
Ethan Harper <98506650+ProtectMe427@users.noreply.github.com>
2023-11-14 14:56:17 -0600

    Update Console.java
    Made some boilerplate to run the Console from an object instance instead of static

--- Console.java
+++ Console.java
@@ -1,1 +1,11 @@
 //For the "frontend"
+public class Console {
+    public Console() {
+        //Placeholder
+    }
+
+    public static void main(String[] args) {
+        Console myConsole = new Console();
+        myConsole.start();
+    }
+}
