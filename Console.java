910d2aafa321ddcb7b640acd98b2ab7ef066de1d
Shreyon Roy <shreyon312@gmail.com>
2023-11-14 15:16:54 -0600

    Delete some comments

--- Console.java
+++ Console.java
@@ -5,12 +5,9 @@ public class Console {
 
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        //java.io.Console console = System.console();
         System.out.println("Please enter username: ");
-        //String username = console.readLine("Username: ");
         String username = input.nextLine();
         System.out.println("Please enter password: ");
-        //String password = new String(console.readPassword("Password: "));
         String password = input.nextLine();
     }
 }
