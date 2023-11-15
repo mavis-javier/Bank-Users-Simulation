2e70aebd9fa63e2e238d68a71293c3b3fba137a6
Ethan Harper <98506650+ProtectMe427@users.noreply.github.com>
2023-11-15 11:50:33 -0600

    Updated Console.java
    Got a little sidetracked and did some extra work on Console

--- Console.java
+++ Console.java
@@ -12,6 +12,26 @@ public class Console {
         String username = input.nextLine();
         System.out.println("Please enter password: ");
         String password = input.nextLine();
+        
+        Subject currentUser = myDBM.login(username, password);
+        if(currentUser == null) {
+            System.out.println("Incorrect login credentials! Please try again.");
+            System.exit(1);
+        }
+        System.out.println("Welcome, " + username);
+
+        System.out.println("Select an option below");
+        int i = 0;
+        for(Option o : optionsMasterList)
+            System.out.println(++i + " - " + o);
+        System.out.println("0 - Logout");
+
+        int selection = input.nextInt();
+        switch(selection) {
+
+        }
+
+        input.close();
     }
 
     
