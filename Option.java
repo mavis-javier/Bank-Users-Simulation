95deaa1a523ca3489865e7401a6fbcdca701869c
Ethan Harper <98506650+ProtectMe427@users.noreply.github.com>
2023-11-15 18:45:33 -0600

    Update Option.java
    Built accessor methods
    
    Made abstract start() method for specific options to implement (i.e. AccessSavingsAccount.start();)

--- Option.java
+++ Option.java
@@ -1,8 +1,20 @@
-public class Option {
+public abstract class Option {
     private SecLevel classification;
     private String name;
 
+    public void setClassification(SecLevel classification) {
+        this.classification = classification;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SecLevel getClassification() {
+        return this.classification;
+    }
     public String toString() {
         return name;
     }
+
+    public abstract void start();
 }
