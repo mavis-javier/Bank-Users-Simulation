28297400d0f12be2977f1ccff77742a2ab4ac7b9
JiaruiZ <123776546+BuggieZ@users.noreply.github.com>
2023-11-16 17:49:12 -0600

    Connection established, insertion to DB success

--- src/jdbc.java
+++ src/jdbc.java
@@ -2,6 +2,8 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class jdbc {
@@ -11,8 +13,14 @@ public class jdbc {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "310872250";
 
+       
+
+
     public static void main(String[] args) {
         Connection connection = null;
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null; 
 
         try {
             // Register the JDBC driver
@@ -22,20 +30,44 @@ public class jdbc {
             System.out.println("Connecting to database...");
             connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
 
-            // Perform database operations here...
-
             System.out.println("Connected to the database!");
+            //String sqlQuery = "SELECT * FROM account";
+            String sqlQuery = "SELECT * FROM account";
+            preparedStatement = connection.prepareStatement(sqlQuery);
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            // Execute the query and get the result set
+            resultSet = preparedStatement.executeQuery();
+
+            //Check if valid result is retuned
+            if (!resultSet.isBeforeFirst()) {
+                System.out.println("No data found in the result set.");
+            }
+            
+
+        // Process the result set
+        while (resultSet.next()) {
+            // Access columns by name or index, for example:
+            int id = resultSet.getInt("Account_ID");
+            int balance = resultSet.getInt("Balance");
+            // Process other columns as needed
+            System.out.println("ID: " + id + ", balance: " + balance);
+        }
+           
+
+       } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
             try {
-                // Close the connection in the finally block to ensure it gets closed even if an exception occurs
+                // Close resources in the finally block
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 if (connection != null) {
                     connection.close();
-                    System.out.println("Connection closed.");
+                    System.out.println("connection closed");
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -43,4 +75,3 @@ public class jdbc {
         }
     }
 }
-
