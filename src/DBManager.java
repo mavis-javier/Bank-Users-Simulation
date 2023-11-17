0af897090c346f45e2470a23b8d4586e377d7974
Ethan Harper <98506650+ProtectMe427@users.noreply.github.com>
2023-11-17 17:16:34 -0600

    Update DBManager.java
    Migrated some functionality from jdbc to DBManager

--- src/DBManager.java
+++ src/DBManager.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -42,9 +43,6 @@ public class DBManager {
             if(qResults.next()) {
                 out = new Subject(qResults.getString("Username"),
                                     SecLevel.values()[1]);
-                if(qResults.getRow() != 1) {
-                    System.out.println("Warning: Duplicate users in DB");
-                }
             }   //else incorrect login credentials, and out stays null
         }
         catch(SQLException e) {
@@ -89,18 +87,84 @@ public class DBManager {
 
     public boolean insertAccount(int Acc_ID, int balance) {
         boolean out = false;
+        Statement sql = null;
+        String query = String.format("INSERT INTO Account (Account_ID, Balance) VALUES (%s, %d)", Acc_ID, balance);
 
+        try {
+            sql = connection.createStatement();
+            if(sql.executeUpdate(query) > 0)
+                out = true;
+        }
+        catch(SQLException e) {
+            System.out.println("Error retrieving Options from DB");
+        }
+        finally {
+            try {
+                sql.close();
+            }
+            catch(NullPointerException n) {}
+            catch(SQLException s) {
+                System.out.println("Could not close sql Statement after insertAccount attempt. Potential memory leak");
+            }
+        }
         return out;
     }
 
     public Account getAccount(int Acc_ID) {
         Account out = null;
+        Statement sql = null;
+        String query = String.format("SELECT * FROM Account " +
+                                     "WHERE Account_ID = \'%d\';", Acc_ID);
+        ResultSet qResults = null;
+
+        try {
+            sql = connection.createStatement();
+            qResults = sql.executeQuery(query);
+
+            if(qResults.next()) {
+                out = new Account(qResults.getInt("Account_ID"),
+                                  qResults.getInt("Balance"));
+            }
+        }
+        catch(SQLException e) {
+            System.out.println("Error with DB during getAccount");
+        }
+        finally {
+            try {
+                sql.close();    //This implicitly closes qResults if applicable
+            }
+            catch(NullPointerException n) {}    //sql was never successfully instantiated. Nothing to clean up
+            catch(SQLException s) {
+                System.out.println("Could not close sql Statement after login attempt. Potential memory leak");
+            }
+        }
 
         return out;
     }
 
-    public boolean adjustBalance(int Acc_ID, int change) {
+    public boolean saveAccount(Account in) {
         boolean out = false;
+        Statement sql = null;
+        String query = String.format("UPDATE Account SET Balance = %d" +
+                                     "WHERE Account_ID = \'%d\';", in.getBalance(), in.getID());
+
+        try {
+            sql = connection.createStatement();
+            if(sql.executeUpdate(query) > 0)
+                out = true;
+        }
+        catch(SQLException e) {
+            System.out.println("Error with DB during setBalance");
+        }
+        finally {
+            try {
+                sql.close();    //This implicitly closes qResults if applicable
+            }
+            catch(NullPointerException n) {}    //sql was never successfully instantiated. Nothing to clean up
+            catch(SQLException s) {
+                System.out.println("Could not close sql Statement after login attempt. Potential memory leak");
+            }
+        }
 
         return out;
     }
