78ca6787ee65fbec745c04d2b00c130c2a88f26c
Ethan Harper <98506650+ProtectMe427@users.noreply.github.com>
2023-11-17 19:15:54 -0600

    Update ViewUserReceipts.java

--- ViewUserReceipts.java
+++ ViewUserReceipts.java
@@ -1,7 +1,7 @@
 public class ViewUserReceipts extends Option {
     private String uname;
     public ViewUserReceipts(String username) {
-        super(SecLevel.TOP_SECRET, ActionType.READ, "View User Receipts");
+        super(SecLevel.CLASSIFIED, ActionType.READ, "View User Receipts");
     }
 
     public void start() {
