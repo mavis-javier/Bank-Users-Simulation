51db83e5bf6d46b4d23d880e2ec0b7b82d304f0a
Ethan Harper <98506650+ProtectMe427@users.noreply.github.com>
2023-11-17 18:38:10 -0600

    Created withdrawal methods

---
+++ withdrawChecking.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class withdrawChecking extends Option {
+    private String uname;
+    private Scanner kb; //Passed in from Console.java. DO NOT close() it.
+
+    public withdrawChecking(String username, Scanner keyboard) {
+        super(SecLevel.CLASSIFIED, "Withdraw Checking Account");
+        this.uname = username;
+        this.kb = keyboard;
+    }
+
+    public void start() {
+        System.out.print("Enter your withdrawal amount :: ");
+        int amount = -1 * Math.abs(kb.nextInt());
+
+        jdbc.writeCKAccount(uname, amount);
+    }
+}
---
+++ withdrawSavings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class withdrawSavings extends Option {
+    private String uname;
+    private Scanner kb; //Passed in from Console.java. DO NOT close() it.
+
+    public withdrawSavings(String username, Scanner keyboard) {
+        super(SecLevel.CLASSIFIED, "Withdraw Savings Account");
+        this.uname = username;
+        this.kb = keyboard;
+    }
+
+    public void start() {
+        System.out.print("Enter your withdrawal amount :: ");
+        int amount = -1 * Math.abs(kb.nextInt());
+
+        jdbc.writeSVAccount(uname, amount);
+    }
+}
